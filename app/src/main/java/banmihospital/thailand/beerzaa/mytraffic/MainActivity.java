2cb899ee76efc4ac0ff344cb4914db69f4296994
BeerZaa <kittinn78@gmail.com>
2015-12-26 15:01:36 +0700

    Finish Day 1

--- app/src/main/java/banmihospital/thailand/beerzaa/mytraffic/MainActivity.java
+++ app/src/main/java/banmihospital/thailand/beerzaa/mytraffic/MainActivity.java
@@ -1,13 +1,55 @@
 package banmihospital.thailand.beerzaa.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    // Explicit หรือ การประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+
+    } // Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.cow);
+
+                buttonMediaPlayer.start();
+
+            }// event
+        });
+
+    }// buttonController
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+
     }
-}
+
+
+} // Main Class นี่คือ คลาสหลัก
